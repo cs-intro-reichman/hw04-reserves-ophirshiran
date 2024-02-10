54537adda8195e3d7f3898c216d889d9be8f80be
ophirshiran <ophir.shiran@post.runi.ac.il>
2024-02-10 21:12:00 +0200

    hw

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,7 +1,7 @@
 public class ArrayOps {
     public static void main(String[] args) {
-       //int[] arrNew={0,3,2};
-       //System.out.println(findMissingInt(arrNew));
+      // int[] arrNew={1,3,2};
+      // System.out.println(findMissingInt(arrNew));
        //int[] arrNew={6,9,9,8,5,3};
        //System.out.println(secondMaxValue(arrNew));
        //int[] arrNew1={1,2,1,1,3,3};
@@ -10,11 +10,11 @@ public class ArrayOps {
       // int[] arrNew={5,1,1,-2};
        //System.out.println(isSorted(arrNew));
     }
- //  --- 1 3 0
+ //  --- 1 3 2
  //+++   0 0 0 0 
- ///     0 1 0 0
+ ///     0 1 0 0 
  ///     0 1 0 3
- ///     0 1 0 3
+ ///     0 1 2 
     public static int findMissingInt (int [] array) {                          
         int[] arrNew=new int[array.length+1];
         for(int i=0; i < array.length; i++){        
@@ -23,7 +23,7 @@ public class ArrayOps {
         for(int j=1; j < arrNew.length; j++){              
             if(arrNew[j]!=arrNew[j+1]-1) {
                 return arrNew[j]+1;
-            }
+            }else return 0;
         }
         return 0;
     }
